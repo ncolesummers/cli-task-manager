7d41fc8480328c259d16a71fcfe9a0286ca2b9cb
ncolesummers <nsummers72@gmail.com>
2021-01-01 14:01:09 -0800

    Implementing persistence

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ncolesummers72/task-manager-cli
 go 1.15
 
 require (
-	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 )
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"log"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/ncolesummers72/task-manager-cli/cmd"
+	"github.com/ncolesummers72/task-manager-cli/db"
 )
 
 func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Println(err)
+	}
+
+	dbPath := filepath.Join(home, "tasks.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 	cmd.RootCmd.Execute()
 }
